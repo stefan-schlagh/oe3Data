cb6b4635f6552df92a47d556e7200ae3e419eb9c
stefan-schlagh <64643082+stefan-schlagh@users.noreply.github.com>
2021-05-03 21:11:38 +0200

    get number of songs per day

--- oe3crawler.py
+++ oe3crawler.py
@@ -5,13 +5,6 @@ from operator import itemgetter
 
 import requests
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-
 
 def main(argv):
     Oe3Crawler(argv)
@@ -34,109 +27,61 @@ class Oe3Crawler:
         args = parser.parse_args()
 
         mode = args.mode
-        if(mode == "crawlData"):
-            self.crawlData()
-        elif(mode == "fetchData"):
+        if(mode == "fetchData"):
             self.fetchData()
+            self.deleteDuplicates()
             self.writeTracks()
         elif(mode == "analyze"):
             self.readTracks()
             self.deleteDuplicates()
             self.printTracks()
             self.writeIntoCSV("tracks.csv")
-            self.getInterpreters()
-            self.writeInterpretersIntoCSV("interpreters.csv")
-    def crawlData(self):
-
-        self.tracks = []
-
-        chrome_options = Options()    
-        #chrome_options.add_argument("--user-data-dir=chrome-data")
-        driver = webdriver.Chrome('chromedriver.exe',options=chrome_options)
-        
-        driver.get("https://oe3.orf.at/player")
-        # wait for searchbox
-        firstTitleSelector = "li.broadcast-item:nth-child(4) > div:nth-child(2)"
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, firstTitleSelector)))
-
-        cookieAcceptElems = driver.find_elements(By.CSS_SELECTOR, "#ds-accept")
-        if(len(cookieAcceptElems) > 0):
-            cookieAcceptElems[0].click()
-            time.sleep(1)
-
-        daysSelector = ".tabs li.tab:not(.highlights)"
-        daysElems = driver.find_elements(By.CSS_SELECTOR, daysSelector)
-
-        for daysElem in daysElems:
-            dateWeekdayElem = daysElem.find_elements(By.CSS_SELECTOR, ".date-weekday")[0]
-            weekday = dateWeekdayElem.get_attribute("title")
-            monthDayElem = daysElem.find_elements(By.CSS_SELECTOR, ".date-day")[0]
-            monthDay = int(monthDayElem.text.replace(".",""))
-
-            print(weekday)
-            print(monthDay)
-
-            #click on dayElem
-            #WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".tabs li.tab:not(.highlights)")))
-            daysElem.click()
-            #driver.execute_script("arguments[0].click();", daysElem)
-
-            #wait for first broadcast
-            broadcastsSelector = ".broadcasts li.broadcast.available"
-            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, broadcastsSelector)))
-
-            # get list of broadcasts  
-            broadcastElems = driver.find_elements(By.CSS_SELECTOR, broadcastsSelector)
-
-            for broadcastElem in broadcastElems:
-                timeElem = broadcastElem.find_elements(By.CSS_SELECTOR,"time")[0]
-                broadcastTime = timeElem.get_attribute("datetime")
-                titleElem = broadcastElem.find_elements(By.CSS_SELECTOR,".title")[0]
-                broadcastTitle = titleElem.text
-
-                print(broadcastTime)
-                print(broadcastTitle)
-
-                #click on broadcastElem
-                #WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".broadcasts li.broadcast.available")))
-                broadcastElem.click()
-                #driver.execute_script("arguments[0].click();", broadcastElem)
-                time.sleep(1)
-
-                #wait for first broadCastDetail
-                WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".broadcast-detail")))
-
-                # get list of tracks
-                trackSelector = ".broadcast-detail .broadcast-item .track"
-                trackElems = driver.find_elements(By.CSS_SELECTOR, trackSelector)
-
-                for trackElem in trackElems:
-                    try:   
-                        interpreter = trackElem.find_elements(By.CSS_SELECTOR,".interpreter span")[0].text
-                        title = trackElem.find_elements(By.CSS_SELECTOR,".title")[0].text
-                        track = {
-                            "interpreter": interpreter,
-                            "title": title
-                        }
-                        self.Log(track)
-                        self.tracks.append(track)
-                    except Exception as e:
-                        self.Log("Error: " + str(e) + " continuing...")
-
-        driver.close()
+            #self.getInterpreters()
+            #self.writeInterpretersIntoCSV("interpreters.csv")
     
     def writeTracks(self,fileName="tracks.txt"):
         f = open(fileName,"w")
-        f.write(json.dumps(self.tracks))
+        f.write(json.dumps(self.trackDays))
         f.close()
 
     def writeIntoCSV(self,fileName):
         f = open(fileName,"w")
         
-        for track in self.tracks:
-            f.write(str(track["num"]) + ";" + track["title"] + ";" + track["interpreter"] + "\n")
+        rows, cols = (len(self.tracks) + 1, len(self.trackDays) + 2)
+        fields = [[0 for i in range(cols)] for j in range(rows)]
+
+        fields[0][0] = "track"
+        fields[0][1] = "interpreter"
+
+        # write days TODO: format
+        for i in range(len(self.trackDays)):
+            fields[0][i + 2] = self.trackDays[i]["day"]
+
+        # write titles + interpreters
+        for i in range(len(self.tracks)):
+            track = self.tracks[i]
+            fields[i + 1][0] = track["title"]
+            fields[i + 1][1] = track["interpreter"]
+
+        # write nums
+        for i in range(len(self.tracks)):
+            for j in range(len(self.trackDays)):
+                trackDay = self.trackDays[j]
+                track = self.tracks[i]
+                fields[i + 1][j + 2] = self.getTrackNumFromTrackDay(track,trackDay)
+
+        for fieldList in fields:
+            for field in fieldList:
+                f.write(str(field) + ";")
+            f.write("\n")
         
         f.close()
+
+    def getTrackNumFromTrackDay(self,track,trackDay):
+        for trackDayTrack in trackDay["tracks"]:
+            if(trackDayTrack["title"] == track["title"] and trackDayTrack["interpreter"] == track["interpreter"]):
+                return trackDayTrack["num"]
+        return 0
 
     def writeInterpretersIntoCSV(self,fileName):
         f = open(fileName,"w")
@@ -148,14 +93,39 @@ class Oe3Crawler:
     
     def readTracks(self):
         f = open("tracks.txt","r")
-        self.tracks = json.loads(f.read())
+        self.trackDays = json.loads(f.read())
         f.close()
     
     def printTracks(self):
+        self.getTracks()
         for track in self.tracks:
             self.Log(track)
+    
+    def getTracks(self):
+
+        tracks = []
+
+        for trackDay in self.trackDays:
+            for track in trackDay['tracks']:
+                if(not self.isInTracks(tracks,track)):
+                    tracks.append(track)
+        
+        self.tracks = sorted(tracks, key=itemgetter('title')) 
+
+    def isInTracks(self,tracks,_track):
+        for track in tracks:
+            if(track["title"] == _track["title"] and track["interpreter"] == _track["interpreter"]):
+                return True
+        return False
 
     def deleteDuplicates(self):
+        # iterate through each day
+        for trackDay in self.trackDays:
+
+            trackDay["tracks"] = self.deleteTrackDayDuplicates(trackDay["tracks"])
+
+    def deleteTrackDayDuplicates(self,tracks):
+        
         tracksNew = []
 
         def isInTracksNew(track):
@@ -171,7 +141,7 @@ class Oe3Crawler:
             trackNew = getTrackNew(track)
             trackNew["num"] = trackNew["num"] + 1
         
-        for track in self.tracks:
+        for track in tracks:
             if(isInTracksNew(track)):
                 incrementTrack(track)
             else:
@@ -180,8 +150,9 @@ class Oe3Crawler:
                     "title": track["title"],
                     "num": 1
                 })
-        self.tracks = sorted(tracksNew, key=itemgetter('num'), reverse=True) 
-    
+        return tracksNew
+        #return sorted(tracksNew, key=itemgetter('num'), reverse=True)
+
     def getInterpreters(self):
 
         interpreters = []
@@ -199,7 +170,7 @@ class Oe3Crawler:
             interpreter = getInterpreter(track["interpreter"])
             interpreter["num"] = interpreter["num"] + track["num"]
 
-        for track in self.tracks:
+        for track in self.trackDays:
             if(isInInterpreters(track["interpreter"])):
                 incrementInterpreter(track)
             else:
@@ -211,12 +182,15 @@ class Oe3Crawler:
     
     def fetchData(self):
 
-        self.tracks = []
+        self.trackDays = []
 
         broadcastDays = self.fetchBroadcasts()
 
         for broadcastDay in broadcastDays:
-            self.Log(broadcastDay["day"])
+            day = broadcastDay["day"]
+            self.Log(day)
+            # track array of day
+            tracks = []
 
             for broadcast in broadcastDay["broadcasts"]:
                 self.Log(broadcast["title"])
@@ -231,7 +205,12 @@ class Oe3Crawler:
                             "description": broadCastData["description"]
                         }
                         self.Log(songData)
-                        self.tracks.append(songData)
+                        tracks.append(songData)
+            # append day
+            self.trackDays.append({
+                "day": day,
+                "tracks": tracks
+            })
 
     def fetchBroadcasts(self):
         r = requests.get('https://audioapi2.orf.at/oe3/json/4.0/broadcasts?_o=oe3.orf.at')
