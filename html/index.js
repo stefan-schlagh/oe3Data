893b3e9739454e553310a4461956562e862e6a23
stefan-schlagh <64643082+stefan-schlagh@users.noreply.github.com>
2021-05-29 15:18:09 +0200

    interpreters chart

--- html/index.js
+++ html/index.js
@@ -1,59 +1,66 @@
 "use strict";
 (async function(){
 
-    const response = await fetch("./data/top15.csv")
-    const topTracksRaw = CSVToArray(await response.text())
-    const header = topTracksRaw[0]
-    // remove first and last line
-    topTracksRaw.shift()
-    topTracksRaw.pop()
 
-    const topTracks = []
-    for(const track of topTracksRaw){
-        topTracks.push({
-            name: track[1] + " " + track[2],
-            items: track.slice(3)
-        })
-    }
-
-    const chartData = []
-    for(let i = 2;i<header.length;i++){
-        chartData.push([moment(header[i]).week()])
-    }
-    // for each day and track
-    for(let i = 0;i < header.length - 2;i++){
-        for(const track of topTracks){
-            chartData[i].push(parseInt(track.items[i]))
-        }
-    }
-
-    google.charts.load('current', {packages: ['corechart', 'line']});
-    google.charts.setOnLoadCallback(drawBasic);
-
-    function drawBasic() {
-
-        const data = new google.visualization.DataTable();
-        data.addColumn('number', 'X');
+    async function drawChart(path,id,indexLength,getName){
+        const response = await fetch(path)
+        const csv = CSVToArray(await response.text())
+        const header = csv[0]
+        // remove first and last line
+        csv.shift()
+        csv.pop()
         
-        for(const track of topTracks){
-            data.addColumn('number', track.name);
+        const top = []
+        for(const item of csv){
+            top.push({
+                name: getName(item),
+                items: item.slice(indexLength + 1)
+            })
         }
 
-        data.addRows(chartData)
+        const chartData = []
+        for(let i = indexLength;i < header.length;i++){
+            chartData.push([moment(header[i]).week()])
+        }
+        // for each day and item
+        for(let i = 0;i < header.length - indexLength;i++){
+            for(const item of top){
+                chartData[i].push(parseInt(item.items[i]))
+            }
+        }
 
-        const options = {
-            hAxis: {
-              title: 'Week'
-            },
-            vAxis: {
-              title: 'Frequency'
-            },
-            height: 800,
-            width: 1600
-          };
-    
-          var chart = new google.visualization.LineChart(document.getElementById('topTracks-chart'));
-    
-          chart.draw(data, options);
+        google.charts.load('current', {packages: ['corechart', 'line']});
+        google.charts.setOnLoadCallback(drawBasic);
+
+        function drawBasic() {
+
+            const data = new google.visualization.DataTable();
+            data.addColumn('number', 'X');
+            
+            for(const item of top){
+                data.addColumn('number', item.name);
+            }
+
+            data.addRows(chartData)
+
+            const options = {
+                hAxis: {
+                title: 'Week'
+                },
+                vAxis: {
+                title: 'Frequency'
+                },
+                height: 800,
+                width: 1600
+            };
+        
+            var chart = new google.visualization.LineChart(document.getElementById(id));
+        
+            chart.draw(data, options);
     }
+    }
+    // draw tracks chart
+    drawChart("./data/topt15.csv","topTracks-chart",2,track => (track[1] + " " + track[2]))
+    // draw interpreters chart
+    drawChart("./data/topi15.csv","topInterpreters-chart",1,interpreter => (interpreter[1]))
 })(this)
